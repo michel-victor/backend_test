952772b6d96e1d59f1f94d46ba6735df70045478
Michel Victor <mvictor86@gmail.com>
2023-03-02 18:51:33 +0100

    feat: Implement user purchases #13

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,13 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :purchase_option
+  has_one :content, through: :purchase_option
+
+  scope :alives, -> { where created_at: (Time.current - 2.days..Time.current) }
+  scope :purchase_options, -> { map(&:purchase_option) }
+  scope :contents, -> { purchase_options.map(&:content) }
+
+  def alive?
+    Time.current.between?(created_at, created_at + 2.days)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
   has_many :purchases
   has_many :purchase_options, through: :purchases
+  has_many :contents, through: :purchase_options
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP, if: :email? }
+
+  def library
+    purchases.alives
+  end
+
+  def can_purchase?(content)
+    library.contents.exclude? content
+  end
+
+  def purchase(content:, quality:)
+    begin
+      can_purchase?(content) ? purchases.create(purchase_option: PurchaseOption.find_by(content:, quality:)) : I18n.t('user.purchase.already_available_content')
+    rescue
+      I18n.t 'user.purchase.create_error'
+    end
+  end
 end
